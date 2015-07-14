1932b9111c0fa2fac304e85e35adef9beef80738
Ivan Kravets <me@ikravets.com>
2015-07-14 23:36:42 +0300

    Run directive when DOM is ready

--- app/scripts/directives/showformerrors.js
+++ app/scripts/directives/showformerrors.js
@@ -27,29 +27,35 @@
       restrict: 'A',
       require: '^form',
       link: function(scope, element, attrs, formCtrl) {
-        if (attrs.showFormErrors) {
-          scope.$watch(function() {
-            return scope.$eval(attrs.showFormErrors);
-          }, function(newValue, oldValue) {
-            element.toggleClass('has-success',
-              oldValue === true && newValue !== true);
-            element.toggleClass('has-error', newValue === true);
-          });
-        } else {
-          var oInput = angular.element(element[0].querySelector('[name]'));
-          if (!oInput) {
-            return;
-          }
-          oInput.bind('change blur', function() {
+        var _wrap = function() {
+          if (attrs.showFormErrors) {
+            scope.$watch(function() {
+              return scope.$eval(attrs.showFormErrors);
+            }, function(newValue, oldValue) {
+              element.toggleClass('has-success',
+                oldValue === true && newValue !== true);
+              element.toggleClass('has-error', newValue === true);
+            });
+          } else {
+            var oInput = angular.element(element[0].querySelector(
+              '[name]'));
+            if (!oInput) {
+              return;
+            }
             var inputName = oInput.attr('name');
-            element.toggleClass('has-success', !formCtrl[inputName].$invalid);
-            element.toggleClass('has-error', formCtrl[inputName].$invalid);
-          });
-        }
+            oInput.bind('change blur', function() {
+              element.toggleClass('has-success', !formCtrl[
+                inputName].$invalid);
+              element.toggleClass('has-error', formCtrl[inputName].$invalid);
+            });
+          }
 
-        scope.$on('form-reset', function() {
-          element.removeClass('has-success has-error');
-        });
+          scope.$on('form-reset', function() {
+            element.removeClass('has-success has-error');
+          });
+        };
+
+        attrs.$observe('showFormErrors', _wrap);
       }
     };
   }
